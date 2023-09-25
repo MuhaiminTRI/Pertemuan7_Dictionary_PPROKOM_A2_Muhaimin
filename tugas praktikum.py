c022bf9f4347e4aacb60c51b93ce0da97f77c1ea
MuhaiminTRI <142768777+MuhaiminTRI@users.noreply.github.com>
2023-09-25 10:18:39 +0700

    Add files via upload

---
+++ tugas praktikum.py
@@ -0,0 +1,31 @@
+# nomor 1
+# a="PEMROGRAMAN python"
+# print(a)
+
+#nomor 2
+# for i in range(5):
+#     print("#", end="")
+
+# nomor 3
+# student={"nama":"faqih", "umur":26, "tinggi":177.6}
+# student["berat"]=70
+# print(student)
+
+#nomor 4
+# print("hai pprokom: ")
+
+#nomor 5
+# a="20567"
+# print(a[1])
+
+#nomor 6
+# x=3
+# y=2
+# print(x+y)
+
+#nomor 7
+# teks="tara"
+# pengali=3
+
+# for i in teks:
+#     print(i*pengali, end="")
